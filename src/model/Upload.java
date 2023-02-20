4d85fd86e113dd48e6be34d5fdd6a076cee4db5f
camian0 <camian6@outlook.com>
2023-02-19 21:41:01 -0500

    the upload class were added methods to upload file from received path, also a method to consult the file

--- src/model/Upload.java
+++ src/model/Upload.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Upload {
 
+    private File readedFiles;
+
+    public void uploadFile(String path) throws IOException {
+        readedFiles = new File(path);
+    }
+
+    public File getFile() {
+        return this.readedFiles;
+    }
 }
