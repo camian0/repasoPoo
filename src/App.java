72ba2b22a84ed20b06ff530a754e2427c465a94d
camian0 <camian6@outlook.com>
2023-02-21 21:27:30 -0500

    the main method was added the classes for the app to works

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import model.ProcessFile;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Ingresa la ruta de la carpeta: ");
+            String path = scanner.nextLine();
+
+            ProcessFile process = new ProcessFile();
+            process.processFile(path, "arar");
+
+            System.out.println(process.getResults());
+
+        } catch (IOException e) {
+            System.out.println("Por favor verifica la ruta ingresada: " + e.getMessage());
+        }
     }
 }
